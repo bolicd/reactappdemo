85476620f8ca01dd630e86be571009584fef1086
Bola <bolicd@gmail.com>
2018-03-03 14:10:39 +0100

    added redux devtools support

--- client/index.js
+++ client/index.js
@@ -4,11 +4,16 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import routes from './routes';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore((state ={})=>state, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 render(
   <Provider store={store} >
